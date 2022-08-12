f91d4a0c9134c3ce4400088496d37c41c6889120
AnasJMirza <ajmhimself@gmail.com>
2022-08-13 01:29:47 +0500

    works on signin API

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import UserSchema from "../schemas/User.js";
+import User from "../schemas/User.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res, next) => {
@@ -7,10 +7,28 @@ export const signup = async (req, res, next) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
-    const newUser = new UserSchema({ ...req.body, password: hash });
+    const newUser = new User({ ...req.body, password: hash });
     await newUser.save();
-    res.status(200).send("User Created Successfully...")
+    res.status(200).send("User Created Successfully...");
   } catch (error) {
-    next(error)
+    next(error);
+  }
+};
+
+export const signin =  async (req, res) => {
+  try {
+    // Find User
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) {
+        res.status(404).send("User not found!")
+    }
+
+    const isCorrect = await bcrypt.compare(req.body.password, user.password)
+    if(!isCorrect){
+        res.status(400).send("Wrong Credentials!")
+    }
+
+  } catch (error) {
+    next(err);
   }
 };
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/Auth.js';
+import { signup, signin } from '../controllers/Auth.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 
 export default router;
