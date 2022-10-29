f3b427288c6d25280803169ebdadd0b8a94de545
AnasJMirza <ajmhimself@gmail.com>
2022-10-30 01:19:15 +0500

    make it better

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 // app.use(cors());
 // const cors = require('cors');
 const corsOptions ={
-    origin:'https://hosting-6adeb.firebaseapp.com/', 
+    origin:'https://hosting-6adeb.firebaseapp.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
